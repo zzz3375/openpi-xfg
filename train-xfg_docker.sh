530fc4583d3f75ab1b65f98c68e7117f2c7089a2
luz211 <zlt1213@gmail.com>
2026-01-13 09:56:30 +0800

    1

--- train-xfg_docker.sh
+++ train-xfg_docker.sh
@@ -1,2 +1,2 @@
-# uv run scripts/compute_norm_stats.py --config-name pi05_xfg
-XLA_PYTHON_CLIENT_MEM_FRACTION=0.95 uv run scripts/train.py pi05_xfg --exp-name=pi05_xfg --overwrite
+# uv run scripts/compute_norm_stats.py --config-name pi05_xfg_docker
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.95 uv run scripts/train.py pi05_xfg_docker --exp-name=pi05_xfg --overwrite
