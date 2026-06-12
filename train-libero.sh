9cb9734dc5557c6a7bdcdfdeee33d22ac8cdeea5
zzz3375 <13694863375@163.com>
2026-06-12 16:50:29 +0800

    1

--- train-libero.sh
+++ train-libero.sh
@@ -1,4 +1,6 @@
 export HF_HOME=/mnt/c/Users/13694/hf_home
 export OPENPI_DATA_HOME=/mnt/c/Users/13694/openpi_data_home
 # XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 WANDB_MODE=offline uv run scripts/compute_norm_stats.py --config-name=pi0_libero_low_mem_finetune
-XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 WANDB_MODE=offline uv run scripts/train.py pi0_libero_low_mem_finetune --exp-name=pi0_libero_low_mem_finetune --overwrite
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 WANDB_MODE=offline &&\ 
+    uv run scripts/train.py pi0_libero_low_mem_finetune &&\
+    --exp-name=pi0_libero_low_mem_finetune --assets-base-dir=/mnt/c/Users/13694/openpi_data_home/assets --overwrite
