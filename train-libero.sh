17f0f288017f7c6973ac862996ccca03d9b938ad
zzz3375 <13694863375@163.com>
2026-06-12 16:48:07 +0800

    1

--- train-libero.sh
+++ train-libero.sh
@@ -1,3 +1,4 @@
-# export OPENPI_DATA_HOME=/root/private_data/robot_ws/openpi_cache
-# XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 WANDB_MODE=offline HF_ENDPOINT=https://hf-mirror.com uv run scripts/train.py pi0_libero_low_mem_finetune --exp-name=pi0_libero_low_mem_finetune --overwrite
+export HF_HOME=/mnt/c/Users/13694/hf_home
+export OPENPI_DATA_HOME=/mnt/c/Users/13694/openpi_data_home
+# XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 WANDB_MODE=offline uv run scripts/compute_norm_stats.py --config-name=pi0_libero_low_mem_finetune
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 WANDB_MODE=offline uv run scripts/train.py pi0_libero_low_mem_finetune --exp-name=pi0_libero_low_mem_finetune --overwrite
