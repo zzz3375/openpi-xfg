39e7e2e468066960bb8b6e83ca015358f3b01e79
zzz3375 <13694863375@163.com>
2026-07-01 13:57:34 +0800

    1

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -989,14 +989,14 @@ _CONFIGS = [
         lr_schedule=_optimizer.CosineDecaySchedule(
             warmup_steps=1_000,
             peak_lr=5e-5,
-            decay_steps=120_000,
+            decay_steps=100_000,
             decay_lr=5e-6,
         ),
-        num_train_steps=200_000,
+        num_train_steps=400_000,
         batch_size=16,
         log_interval=100,
-        save_interval=20_000,
-        keep_period=20_000,
+        save_interval=40_000,
+        keep_period=40_000,
         num_workers=0,
         freeze_filter=nnx.Not(
             nnx_utils.PathRegex(".*(llm.*_1|action_in_proj|action_out_proj|time_mlp_in|time_mlp_out|PaliGemma.*img).*")
